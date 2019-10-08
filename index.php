f55505f96e5f242daf8750bc07cf072c093ff4ef
Alexandr Korneychuk <systemnet@yandex.ru>
2019-10-08 17:23:59 +0300

    parse G-Store

--- index.php
+++ index.php
@@ -9,16 +9,3 @@ $checker = new PriceChecker();
 $checker->setMarketplace(new GStore());
 
 dd($checker->getPrice());
-
-
-//file_put_contents('test.txt',$page);
-//$pq=new PhpQuery;
-//$pq->load_str($page);
-
-//$res = $pq->query('.woocommerce-Price-amount.amount.final-price');
-
-//foreach ($res as $item) {
-//    print_r($item); echo '<br>';
-//}
-//echo $page;
-
